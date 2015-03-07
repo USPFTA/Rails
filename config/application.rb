24b7208d31eebef363f7bb8ce4139625a609991f
Matt Huhman <huhmanbeing@gmail.com>
2015-03-07 15:32:32 -0500

    Just for shits and giggles

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module TiyHackathon
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete]
       end
     end
   end
