1178cbaced569aa4998591573beccff8805719ce
Matt Huhman <huhmanbeing@gmail.com>
2015-03-06 23:26:03 -0500

    rack-cors should definitely be fixed now, forgot an end

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,10 @@ module TiyHackathon
     config.active_record.raise_in_transactional_callbacks = true
 
     config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-      origins '*'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
     end
   end
 end
