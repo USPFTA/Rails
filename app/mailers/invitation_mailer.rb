53d5f10dc9fc8735288199d5f777544e97174b18
Matt Huhman <huhmanbeing@gmail.com>
2015-03-08 12:45:29 -0400

    removed binding.pry

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,7 +6,6 @@ class InvitationMailer < ApplicationMailer
     @sender = sender
     @recipient = recipient
     @game = game
-    binding.pry
     mail(to: @recipient.email, subject: 'You have been invited to play!')
   end
 
