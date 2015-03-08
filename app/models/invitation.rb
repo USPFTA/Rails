51440027810d0a63a191a87e939ea27b5a9cbeaf
Matt Huhman <huhmanbeing@gmail.com>
2015-03-08 09:48:40 -0400

    commented out mailer to test invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,7 +3,7 @@ class Invitation < ActiveRecord::Base
   belongs_to :invited, class_name: "User"
   belongs_to :game
 
-  after_create :send_invitation_email
+  #after_create :send_invitation_email
 
   def send_invitation_email
     InvitationMailer.invitation_email(self.inviter, self.invited, self.game).deliver_now
